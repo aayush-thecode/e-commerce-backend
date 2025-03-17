df4bb02c29c3fdbfe87e1dfe340ef31918b7a015
Aayush Pradhan <muma5581@gmail.com>
2025-03-17 13:43:08 +0545

    backend

--- src/config/database.config.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export default function connectDatabase(url: string) {
-console.log("🚀 ~ connectDatabase ~ url:", url)
-
-    mongoose.connect(url)
-    .then(()=>{console.log('Database connected.')})
-    .catch((err)=>console.log('db connect error', err))
-}
